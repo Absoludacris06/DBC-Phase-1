51fb4cf2e573e7082b9a8c046eb1d6fc1e83f422
Absoludacris06 <nicholas.saldivar@gmail.com>
2013-10-14 17:45:11 -0700

    refactor orange tree

--- week_2/orange_tree.rb
+++ week_2/orange_tree.rb
@@ -1,75 +1,80 @@
+# Orange tree exercise.  Beginning of OOP.
+
 class NoOrangesError < StandardError
 end
- 
+
 class OrangeTree
- 
-  attr_reader :number_of_oranges
+
+  attr_reader :number_of_oranges, :height
   MATURE = 5
   DEAD = 15
- 
+
   def initialize
     @number_of_oranges = 0
+    @height = 3
   end
 
   def age!
-    @age ||= 0
     @age += 1
-    if @age >= MATURE && @age < DEAD
-      @number_of_oranges += 5
+    @height += 1
+    if age >= MATURE && !self.dead?
+      @number_of_oranges += (20..40).to_a.sample
     end
   end
- 
+
   def age
     @age ||= 0
   end
- 
+
   def dead?
-    @age >= DEAD
-  end 
- 
-  def any_oranges?
-    MATURE <= @age && @age < DEAD && @number_of_oranges > 0
+    age >= DEAD
   end
- 
+
+  def any_oranges?
+    age < DEAD && @number_of_oranges > 0
+  end
+
   def pick_an_orange!
     raise NoOrangesError, "This tree has no oranges" unless self.any_oranges?
     @number_of_oranges -= 1
-    p "You picked an orange"
     Orange.new
   end
- 
+
 end
- 
+
 class Orange
- 
+
   MAX_DIA = 4
   MIN_DIA = 2
   attr_reader :diameter
- 
+
   def initialize
     @diameter = rand * (MAX_DIA - MIN_DIA) + MIN_DIA
   end
- 
+
 end
- 
-new_tree = OrangeTree.new
-new_tree.age!
-p new_tree.age
-new_tree.age!
-p new_tree.age
-new_tree.age!
-p new_tree.age
-new_tree.age!
-p new_tree.age
-p new_tree.any_oranges?
-new_tree.age!
-p new_tree.any_oranges?
-p new_tree.number_of_oranges
-fresh_orange = new_tree.pick_an_orange!
-p new_tree.number_of_oranges
-p fresh_orange.diameter
-until new_tree.age > 15
-  new_tree.age!
+
+tree = OrangeTree.new
+
+tree.age! until tree.any_oranges?
+
+puts "Tree is #{tree.age} years old and #{tree.height} feet tall"
+
+until tree.dead?
+  basket = []
+
+  while tree.any_oranges?
+    basket << tree.pick_an_orange!
+  end
+
+  avg_diameter = basket.inject(0) { |sum, orange| sum + orange.diameter } / basket.size
+
+  puts "Year #{tree.age} Report"
+  puts "Tree height: #{tree.height} feet"
+  puts "Harvest:     #{basket.size} oranges with an average diameter of #{avg_diameter.round(2)} inches"
+  puts ""
+
+  tree.age!
 end
-p new_tree.age
-p new_tree.dead?
+
+puts "Alas, the tree, she is dead!"
